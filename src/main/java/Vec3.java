6a74cc47cd99bbf915910816d6cdaff169f22601
Roman Derin <derinrom@gmail.com>
2022-09-30 14:36:36 +0500

    upd&add some methods in Vec3

--- src/main/java/Vec3.java
+++ src/main/java/Vec3.java
@@ -13,15 +13,14 @@ public class Vec3 {
     public double z() {return e[2];}
 
     public Vec3 add(Vec3 v) {
-        return new Vec3(e[0] += v.e[0], e[1] += v.e[1], e[2] += v.e[2]);
+        return new Vec3(e[0] + v.e[0], e[1] + v.e[1], e[2] + v.e[2]);
     }
 
-    public Vec3 multiply(double t) {
-        return new Vec3(e[0] *= t, e[1] *= t, e[2] *= t);
+    public Vec3 mul(double t) {
+        return new Vec3(e[0] * t, e[1] * t, e[2] * t);
     }
-
-    public Vec3 divide(double t) {
-        return new Vec3(e[0] *= 1/t, e[1] *= 1/t, e[2] *= 1/t);
+    public Vec3 div(double t) {
+        return new Vec3(e[0] / t, e[1] / t, e[2] / t);
     }
 
     public double length() {
@@ -29,6 +28,26 @@ public class Vec3 {
     }
     public  double length_squared() {
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
+
+    public void set(Vec3 v) {
+        e[0] = v.e[0];
+        e[1] = v.e[1];
+        e[2] = v.e[2];
+    }
+
+    // Vec3 Utility methods
+
+    public Vec3 sub(Vec3 v) {
+        return new Vec3(e[0] - v.e[0], e[1] - v.e[1], e[2] - v.e[2]);
+    }
+
+    public Vec3 mul(Vec3 v) {
+        return new Vec3(e[0] * v.e[0], e[1] * v.e[1], e[2] * v.e[2]);
+    }
+
+    public Vec3 div(Vec3 v) {
+        return new Vec3(e[0] / v.e[0], e[1] / v.e[1], e[2] / v.e[2]);
     }
 
     public double dot(Vec3 u, Vec3 v) {
@@ -44,6 +63,6 @@ public class Vec3 {
     }
 
     public Vec3 unit_vector(Vec3 v) {
-        return v.divide(v.length());
+        return v.div(v.length());
     }
 }
