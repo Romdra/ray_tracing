5bfeb28eac93aaf33e632aff9ca9723349d79f5e
Roman Derin <derinrom@gmail.com>
2022-10-04 17:59:48 +0500

    add class Ray

---
+++ src/main/java/Ray.java
@@ -0,0 +1,18 @@
+public class Ray {
+
+    Vec3 dir;
+    Vec3 orig;
+
+    public Ray() {};
+    public Ray(Vec3 origin, Vec3 direction) {
+        this.orig = origin;
+        this.dir = direction;
+    };
+
+    public Vec3 origin() {return orig;}
+    public Vec3 direction() {return dir;}
+
+    public Vec3 linePosition(double t) {
+        return dir.add(orig.mul(t));
+    }
+}
