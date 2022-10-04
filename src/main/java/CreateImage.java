8614d76bf5b39f121bbeafbdbd4851b162402c62
Roman Derin <derinrom@gmail.com>
2022-10-04 17:18:23 +0500

    upd methods in Color class

--- src/main/java/CreateImage.java
+++ src/main/java/CreateImage.java
@@ -1,11 +1,4 @@
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
-import java.util.List;
-
+import java.io.*;
 public class CreateImage {
 
     public static void main(String[] args) throws IOException {
@@ -13,19 +6,30 @@ public class CreateImage {
         final int imageWidth = 256;
         final int imageHeight = 256;
 
-        String create = ("P3\n" + imageWidth + " " + imageHeight + "\n255\n");
-        List<String> rows = Arrays.asList(create);
-        Files.write(Paths.get("/home/roman/Documents/Projects/Image.ppm"),
-                rows,
-                StandardCharsets.UTF_8,
-                StandardOpenOption.CREATE,
-                StandardOpenOption.APPEND);
+
+        try(FileWriter writer = new FileWriter("Image.ppm", true))
+        {
+            String create = ("P3\n" + imageWidth + " " + imageHeight + "\n255\n");
+            writer.write(create);
+            writer.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
 
         for (int k = imageHeight - 1; k >= 0; --k) {
             for (int i = 0; i < imageWidth; ++i) {
                 Color pixelColor = new Color((double)i / (imageWidth - 1), (double)k / (imageHeight - 1), 0.25);
-                pixelColor.writeColor(pixelColor);
+                try(FileWriter writer = new FileWriter("Image.ppm", true))
+                {
+                    writer.write(pixelColor.toString(pixelColor));
+                    writer.flush();
+                }
+                catch(IOException ex){
+                    System.out.println(ex.getMessage());
+                }
             }
         }
+
     }
 }
