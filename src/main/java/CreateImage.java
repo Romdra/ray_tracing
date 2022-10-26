0381fbb7584023a0d26d4955faab8e8e92e050c0
Roman Derin <derinrom@gmail.com>
2022-10-26 20:46:25 +0500

    Complete step_7

--- src/main/java/CreateImage.java
+++ src/main/java/CreateImage.java
@@ -22,31 +22,28 @@ public class CreateImage {
 
         for (int j = imageHeight - 1; j >= 0; --j) {
             for (int i = 0; i < imageWidth; ++i) {
-                Color pixelColor = new Color(0,0,0);
+                Vec3 pixelColor = new Vec3(0,0,0);
                 for (int k = 0; k < samplesPerPixel; ++k) {
                     double u = (i + Math.random()) / (imageWidth - 1);
                     double v = (j + Math.random()) / (imageHeight - 1);
                     Ray r = cam.getRay(u, v);
-                    pixelColor = rayColor(r, world);
+                    pixelColor = pixelColor.add(rayColor(r, world));
                 }
-                ImageRecording.myRecord(pixelColor.toString(samplesPerPixel));
+                ImageRecording.myRecord(Color.fromVec3(pixelColor).toString(samplesPerPixel));
             }
         }
     }
 
-    public static Color rayColor(Ray r, Hittable world) {
+    public static Vec3 rayColor(Ray r, Hittable world) {
         HitRecord rec = new HitRecord();
         if(world.hit(r, 0, INFINITY, rec)) {
-            Vec3 color = new Vec3(1,1,1).add(rec.normal).mul(0.5);
-            return Color.fromVec3(color);
+            return new Vec3(1,1,1).add(rec.normal).mul(0.5);
         }
 
         Vec3 unitDirection = Vec3.unitVector(r.direction());
         double t = 0.5 * (unitDirection.y() + 1.0);
-        Vec3 rc = new Vec3(1.0, 1.0, 1.0).mul(1.0 - t)
+        return new Vec3(1.0, 1.0, 1.0).mul(1.0 - t)
                 .add(new Vec3(0.5, 0.7, 1.0).mul(t));
-
-        return Color.fromVec3(rc);
     }
 
     public static double degrees_to_radians(double degrees) {
